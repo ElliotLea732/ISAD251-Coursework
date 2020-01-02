ef989fdb12cea69e134742286587eac38b1f8b31
Elliot Lea <elliot.lea@students.plymouth.ac.uk>
2020-01-02 20:32:39 +0000

    Finished home page

---
+++ CafeWebApp/Controllers/DrinksMenuController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CafeWebApp.Controllers
+{
+    public class DrinksMenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
